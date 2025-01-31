05f9d14e64637448977ba05d923e47f744ea5e21
maksim.naumov1889@gmail.com <maksim.naumov1889@gmail.com>
2025-01-31 16:47:56 +0300

    vim-tmux-navigator

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,9 @@ map("i", "kj", "<Esc>", { noremap = true, silent = true })
 map("n", ";w", ":w<CR>", { noremap = true, silent = true })
 map("i", ";w", "<Esc>:w<CR>", { noremap = true, silent = true })
 map("v", ";w", "<Esc>:w<CR>", { noremap = true, silent = true })
+
+-- Vim Tmux Navigate. Navigate between nvim and tmux using ctrl+hjkl
+map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { noremap = true, silent = true, desc = "Window left" })
+map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { noremap = true, silent = true, desc = "Window right" })
+map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { noremap = true, silent = true, desc = "Window down" })
+map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { noremap = true, silent = true, desc = "Window up" })
