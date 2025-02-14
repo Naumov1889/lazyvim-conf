34c56bd9ec2fd03cf584b674be6ba23e53b52cce
maksim.naumov1889@gmail.com <maksim.naumov1889@gmail.com>
2025-02-14 11:32:23 +0300

    update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,3 +18,16 @@ map("n", "<C-h>", "<cmd>TmuxNavigateLeft<CR>", { noremap = true, silent = true, 
 map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { noremap = true, silent = true, desc = "Window right" })
 map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { noremap = true, silent = true, desc = "Window down" })
 map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { noremap = true, silent = true, desc = "Window up" })
+
+-- Close buffer using shift+q
+map("n", "Q", "<cmd>bd<CR>", { noremap = true, silent = true, desc = "Close buffer" })
+
+-- When pasting, don't yank selected text
+map("v", "p", '"_dP', { noremap = true, silent = true, desc = "Paste without yanking" })
+
+-- When deleting, don't yank selected text
+map("v", "d", '"_d', { noremap = true, silent = true, desc = "Delete without yanking" })
+
+-- When deleting a whole line, don't yank it
+map("n", "dd", '"_dd', { noremap = true, silent = true, desc = "Delete line without yanking" })
+
